93c80fc02dde24759ccb8db3d64b64aaefc40a27
tainahvinha <tainah_vinha@hotmail.com>
2021-05-07 22:33:37 -0300

    atual

--- java/AULAS/src/Lista4/Lista4ex1.java
+++ java/AULAS/src/Lista4/Lista4ex1.java
@@ -5,33 +5,30 @@ import java.util.Scanner;
 public class Lista4ex1 {
 
 	public static void main(String[] args) {
-		
-		//1- Faça um programa que possua um vetor denominado A que armazene 6 números inteiros. O programa deve executar os seguintes passos: 
-			//(a) Atribua os seguintes valores a esse vetor: 1, 0, 5, -2, -5, 7. 
-			//(b) Armazene em uma variável inteira (simples) a soma entre os valores das posições A[0], A[1] e A[5] do vetor e mostre na tela esta soma. 
-			//(c) Modifique o vetor na posição 4, atribuindo a esta posição o valor 100. 
-			//(d) Mostre na tela cada valor do vetor A, um em cada linha.
 
-		
-		
-		Scanner leia = new Scanner (System.in);
-		int a[] = {1, 0, 5, -2, -5, 7};
+		// 1- Faça um programa que possua um vetor denominado A que armazene 6 números
+		// inteiros. O programa deve executar os seguintes passos:
+		// (a) Atribua os seguintes valores a esse vetor: 1, 0, 5, -2, -5, 7.
+		// (b) Armazene em uma variável inteira (simples) a soma entre os valores das
+		// posições A[0], A[1] e A[5] do vetor e mostre na tela esta soma.
+		// (c) Modifique o vetor na posição 4, atribuindo a esta posição o valor 100.
+		// (d) Mostre na tela cada valor do vetor A, um em cada linha.
+
+		Scanner leia = new Scanner(System.in);
+		int a[] = { 1, 0, 5, -2, -5, 7 };
 		int soma = 0;
-				
-		
-		
-		a[4]=100;
-		
-				soma = (a[0]+a[1]+a[5]);
-			System.out.printf("A soma das posições A[0], A[1] e A[5] é : %d\n", soma);
-			
-			System.out.println("A relação dos vetores é: ");
-			for(int x=0;x<6;x++){
-				System.out.println(a[x]);
-										
-			}
-		
+
+		a[4] = 100;
+
+		soma = (a[0] + a[1] + a[5]);
+		System.out.printf("A soma das posições A[0], A[1] e A[5] é : %d\n", soma);
+
+		System.out.println("A relação dos vetores é: ");
+		for (int x = 0; x < 6; x++) {
+			System.out.println(a[x]);
+
+		}
+
 	}
 
 }
-	
--- java/AULAS/src/Lista4/Lista4ex3.java
+++ java/AULAS/src/Lista4/Lista4ex3.java
@@ -5,20 +5,40 @@ import java.util.Scanner;
 public class Lista4ex3 {
 
 	public static void main(String[] args) {
-	
-		//3- Leia uma matriz 3 x 3, conte e escreva quantos valores maiores que 10 ela possui.
 
-		Scanner leia = new Scanner (System.in);
-		
-		int matriz [][] = new int [3][3];
-		
-			for (int linha =0; linha<3;linha++) {
-				for(int coluna=0; coluna<3; coluna++) {
-					System.out.printf("Insira o elemento M[%d][%d]: ");
-					matriz[linha][coluna] = leia.next()
+		// 3- Leia uma matriz 3 x 3, conte e escreva quantos valores maiores que 10 ela
+		// possui.
+
+		Scanner leia = new Scanner(System.in);
+
+		int matriz[][] = new int[3][3];
+		int maiorQue10 = 0;
+
+		for (int linha = 0; linha < 3; linha++) {
+			for (int coluna = 0; coluna < 3; coluna++) {
+				System.out.printf("Insira o elemento M[%d][%d]: ", (linha + 1), (coluna + 1));
+				matriz[linha][coluna] = leia.nextInt();
+
+				if (matriz[linha][coluna] > 10) {
+					maiorQue10++;
 				}
 			}
-		
+		}
+
+		System.out.println();
+		System.out.println("Resultado da Matriz:\n");
+
+		for (int linha = 0; linha < 3; linha++) {
+			for (int coluna = 0; coluna < 3; coluna++) {
+				System.out.printf("\t %d \t", matriz[linha][coluna], "\n\n");
+
+			}
+			System.out.println();
+		}
+
+		System.out.printf("\nForam informados: %d números maiores que 10", maiorQue10);
+
+		leia.close();
 
 	}
 
